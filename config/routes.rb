d2d33ab1d68bd5b07eecdfe8d2ab6bd0b525ac94
MarchenkoAndrey61 <xayc755@yandex.ru>
2019-10-14 16:49:41 +0300

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   
+  # get 'users/index'
+  # get 'users/:id', to: 'users#show', as: :users_show_path
   devise_for :users
   root to: 'phrases#index', as: :root_path
 
-  get 'phrases/new'
-  get 'phrases/create'
-  get 'phrases/index'
+  # get 'phrases/new'
+  # get 'phrases/create'
+  # get 'phrases/index'
 
   root "static_pages#hello"
-
-  resource :phrases
-
+  resources :users
+  resources :phrases
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
